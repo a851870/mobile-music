d5e0490fe90c0e364d939b7f1e61c765e43aeee5
a851870 <851870124@qq.com>
2018-10-10 00:33:10 +0800

    Solve the first screen effect

--- src/mixin/scrollMixin.js
+++ src/mixin/scrollMixin.js
@@ -1,6 +1,19 @@
 import BScroll from 'better-scroll'
 const mixinScroll = {
   mounted () {
+    /*
+     *  BScroll
+     *  @parma bounceTime 动画延迟设置,ps: 动画未结束click无效
+     *  @parma startY 初始滑动位置
+     */
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      probeType: 3,
+      bounceTime: 400,
+      startY: -90,
+      taps: true,
+      click: true
+    })
+    console.log(this.scroll)
     // this.$refs.container.addEventListener('touchend', () => {
     //   this.$refs.container.style['padding-top'] = this.transformDelta + 'px'
     // }, false)
@@ -20,9 +33,8 @@ const mixinScroll = {
 //     }
 //   }
   updated () {
-    this.scroll = new BScroll(this.$refs.wrapper, {
-      click: true
-    })
+
+
     // this.$nextTick(() => {
     // //   debugger
     // //   if (this.ldDataFinished) {
